1ca19cb5c465199a62bcae2c19f205a66af11098
jooyeal <jyol1234@gmail.com>
2022-05-20 13:32:29 +0900

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>YORU</title>
+        <meta name="description" content="yoru's homepage" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>hello</div>
+    </div>
+  );
+};
+
+export default Home;
