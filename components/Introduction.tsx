b654817c67b0af547d95343d335908a9d5ac41de
jooyeal <jyol1234@gmail.com>
2022-05-24 11:59:51 +0900

    escape

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -75,13 +75,13 @@ function Introduction({ sectionRef }: Props) {
           className="w-1/2 p-4 font-introduction overflow-auto hidden-scroll break-all mobile:text-xs"
           style={{ height: "calc(100vh - 10rem)" }}
         >
-          Hello i'm Juyeol Cha. i'm working as webdeveolpor in japan. though i
+          {`Hello i'm Juyeol Cha. i'm working as webdeveolpor in japan. though i
           lived in SouthKorea until i graduate from university, i came to Japan
           because i love japanese culture and food. especially i love japanese
           pub called IZAKAYA which can fill pretty old-fashion when SHOWA mood.
           i'm a bilingual now who can speak Korean and Japanese. i'm studying
           English also to become a trilingual. i love my job as deveoloper
-          because i like to make things.
+          because i like to make things.`}
         </div>
       </div>
     </Section>
