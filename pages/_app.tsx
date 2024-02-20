a7bf914d6ad0011a861c445f20f8c2eff260cafb
jooyeal <34053954+jooyeal@users.noreply.github.com>
2024-02-20 18:44:21 +0900

    fix: defaultTheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import SideMenu from "../components/SideMenu";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark">
+    <ThemeProvider attribute="class" defaultTheme="light">
       <NextNprogress
         color="#ab47bc"
         startPosition={0.3}
